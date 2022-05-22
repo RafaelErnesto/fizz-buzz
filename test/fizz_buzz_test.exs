47dc94cc2a303ba8be806bd39bfaac81ba36e2f8
Rafael <rflernesto@gmail.com>
2022-05-22 16:19:02 -0400

    chore: format code

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -5,6 +5,7 @@ defmodule FizzBuzzTest do
     test "when a valid file is provided, returns the converted list" do
       assert FizzBuzz.build("numbers.txt") == [1, 2, :fizz, 4, :buzz, :buzz, :buzz, :fizzbuzz]
     end
+
     test "when a invalid file is provided, returns an error" do
       assert FizzBuzz.build("invalid.txt") == "Error reading the file enoent"
     end

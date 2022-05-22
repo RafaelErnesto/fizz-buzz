740c15378f909498b2572dc21ee693d880464684
Rafael <rflernesto@gmail.com>
2022-05-22 16:17:14 -0400

    test: add tests

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,9 +1,12 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  
+
   describe "build/1" do
     test "when a valid file is provided, returns the converted list" do
-      assert FizzBuzz.build("numbers.txt") == :banana
+      assert FizzBuzz.build("numbers.txt") == [1, 2, :fizz, 4, :buzz, :buzz, :buzz, :fizzbuzz]
+    end
+    test "when a invalid file is provided, returns an error" do
+      assert FizzBuzz.build("invalid.txt") == "Error reading the file enoent"
     end
   end
 end
